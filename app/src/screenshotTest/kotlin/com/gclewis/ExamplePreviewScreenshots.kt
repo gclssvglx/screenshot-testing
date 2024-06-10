bb710c356ed5649b14ad931bc0be946fdb23c75a
Graham Lewis <44037625+gclssvglx@users.noreply.github.com>
2024-06-10 09:37:49 +0100

    Run update and validate tasks

---
+++ app/src/screenshotTest/kotlin/com/gclewis/ExamplePreviewScreenshots.kt
@@ -0,0 +1,15 @@
+package com.gclewis
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.gclewis.ui.theme.ScreenshotTestingTheme
+
+class ExamplePreviewScreenshots {
+    @Preview(showBackground = true)
+    @Composable
+    fun GreetingPreview() {
+        ScreenshotTestingTheme {
+            Greeting("Android")
+        }
+    }
+}
